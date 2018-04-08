d3ce7e7fda632c9924c519415bd731243cd78ce2
wehelie <mawehelie@gmail.com>
2018-04-08 02:29:00 -0700

    1 test passed :heavy_check_mark:

--- app/src/main/java/com/example/wehelie/helloworld/MainActivity.java
+++ app/src/main/java/com/example/wehelie/helloworld/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        authorName = findViewById(R.id.author);
+        authorName = findViewById(R.id.authorTextView);
     }
 
     /**
