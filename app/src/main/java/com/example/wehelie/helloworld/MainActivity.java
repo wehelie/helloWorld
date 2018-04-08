4ec82ec246d42fe16e65392782d972472d989940
wehelie <mawehelie@gmail.com>
2018-04-08 02:39:18 -0700

    1 test passed :octocat:

--- app/src/main/java/com/example/wehelie/helloworld/MainActivity.java
+++ app/src/main/java/com/example/wehelie/helloworld/MainActivity.java
@@ -7,19 +7,27 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // reference variable to textView item
     private TextView authorName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // info return to app if activity is destroyed for unknown reason
         super.onCreate(savedInstanceState);
+
+        // set content on the main view
         setContentView(R.layout.activity_main);
+
+        // grab textView item by id reference
         authorName = findViewById(R.id.authorTextView);
     }
 
     /**
+     * this method willl be called upon click of butotn
      * sets the author name and date to the textview
      */
     public void getAuthor(View view) {
+        // set text to the textView
         authorName.setText(R.string.text_info);
     }
 }
